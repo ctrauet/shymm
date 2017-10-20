db604b6f82e18a851a561f8bfdba829f3eef35dc
couespel <32844452+couespel@users.noreply.github.com>
2017-10-20 11:47:12 +0200

    matinée 5jjj

--- script2.js
+++ script2.js
@@ -8,10 +8,8 @@ $.getJSON("https://api.airtable.com/v0/appbd7eQPE8MGwj1F/musics?api_key=keywjwrn
 function(data) {
    console.log(data);
    $.each(data.records, function (key, val) {
-       console.log(val.fields.title);
-       $('<class>'+val.fields.title+'</class>').appendTo('#musiques');
        $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
-}); 
+});
 
 }
 );
