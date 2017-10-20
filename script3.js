35323232252ce2b40bb8de2b90bf4b9148f8e8bc
couespel <32844452+couespel@users.noreply.github.com>
2017-10-20 12:58:41 +0200

    mi journée thibaud 5

---
+++ script3.js
@@ -0,0 +1,10 @@
+$.getJSON("https://api.airtable.com/v0/appJBxSmyHMXbstSJ/musics?api_key=keyzOxUWBfRnJ4LUA",
+function(data) {
+console.log(data);
+$.each(data.records, function(key,val){
+    
+$("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#toplist');
+    
+});
+}
+);
